b9dd1b588915300b5fbdb7e038f7a190092c31a7
mehdiabbaszaidi65-maker <mehdiabbaszaidi65@gmail.com>
2026-06-12 22:04:28 +0530

    Create Longest_Palindromic_Substring.cpp

---
+++ WEEK_2/DAY_5/Longest_Palindromic_Substring.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+string res="";
+    string longestPalindrome(string s) {
+        int n=s.length();
+        int maxi=0;
+        int start=0;
+        for(int i=0;i<s.length();i++)
+        {
+            int l=i;
+            int r=i;
+            while(l>=0&&r<n&&s[l]==s[r])
+            {
+                if(r-l+1>maxi)
+                {
+                    maxi=r-l+1;
+                    start=l;
+                }
+                l--;
+                r++;
+            }
+            l=i;
+            r=i+1;
+            while(l>=0&&r<n&&s[l]==s[r])
+            {
+                if(r-l+1>maxi)
+                {
+                    maxi=r-l+1;
+                    start=l;
+                }
+                l--;
+                r++;
+            }
+            
+        }
+        return s.substr(start,maxi);
+    }
+};
