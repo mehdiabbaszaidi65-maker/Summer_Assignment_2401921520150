f1f665c64ef6007ba0fbc26e2cad5b530afd200d
mehdiabbaszaidi65-maker <mehdiabbaszaidi65@gmail.com>
2026-06-18 00:24:13 +0530

    Create Next_Greater_Element_I.cpp

---
+++ WEEK_3/DAY_3/Next_Greater_Element_I.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int, int> mp;
+
+        for (int num : nums2) {
+            while (!st.empty() && num > st.top()) {
+                mp[st.top()] = num;
+                st.pop();
+            }
+            st.push(num);
+        }
+
+        vector<int> ans;
+
+        for (int num : nums1) {
+            if (mp.find(num) != mp.end())
+                ans.push_back(mp[num]);
+            else
+                ans.push_back(-1);
+        }
+
+        return ans;
+    }
+};
